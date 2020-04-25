b24483cd2b093c50f36219a2ff66404a75fa51e0
mjclawrence <lawrence@middlebury.edu>
2020-04-25 10:47:39 -0400

    create gss workshop

---
+++ data_workshop/base_project_soci301.R
@@ -0,0 +1,8 @@
+required_packages <- c("tidyverse", "rtweet", "kableExtra", "lubridate", "ggthemes",
+                       "Rcpp", "base64enc", "digest", "evaluate", "glue",
+                       "highr", "htmltools", "jsonlite", "knitr", "magrittr",
+                       "markdown", "mime", "rmarkdown", "rprojroot",
+                       "stringi", "stringr", "tinytex", "xfun", "yaml",
+                       "plyr", "lutz", "suncalc", "splines")
+install.packages(required_packages)
+
